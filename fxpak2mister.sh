1ee89795604e1c2b6c370d3e84d793953b55a0ff
Em K IronFist <22283068+Em-K-IronFist@users.noreply.github.com>
2021-05-19 22:09:11 -0400

    Update fxpak2mister.sh

--- fxpak2mister.sh
+++ fxpak2mister.sh
@@ -7,16 +7,16 @@ source=$fxpak_saves
 destination=$mister_saves
 
 # Make backup folder for MiSTer saves to protect from easy overwrites.
-if [ ! -e "$destination/backup" ]
-then
-	mkdir -p "$destination/backup"
-fi
+#if [ ! -e "$destination/backup" ]
+#then
+#	mkdir -p "$destination/backup"
+#fi
 
 cd $source
 for i in *.srm
 do
 	oldname="$i"
 	newname="${oldname/.srm/.sav}"
-	if [ -e "$destination/$newname" ]; then echo "Backing up $newname before updating..."; cp "$destination/$newname" "$destination/backup/$newname"; fi
-	cp -v "$oldname" "$destination/$newname"
+	#if [ -e "$destination/$newname" ]; then echo "Backing up $newname before updating..."; cp "$destination/$newname" "$destination/backup/$newname"; fi
+	mv -v "$oldname" "$destination/$newname"
 done
