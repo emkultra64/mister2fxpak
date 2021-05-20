4a0f507310e7f2539df730885858c09cee09e62a
Em K IronFist <22283068+Em-K-IronFist@users.noreply.github.com>
2021-05-19 22:18:16 -0400

    Update mister2fxpak.sh

--- mister2fxpak.sh
+++ mister2fxpak.sh
@@ -7,16 +7,16 @@ source=$mister_saves
 destination=$fxpak_saves
 
 # Make backup folder for sd2snes saves to protect from easy overwrites.
-#if [ ! -e "$destination/backup" ]
-#then
-#	mkdir -p "$destination/backup"
-#fi
+if [ ! -e "$destination/backup" ]
+then
+	mkdir -p "$destination/backup"
+fi
 
 cd $source
 for i in *.sav
 do
 	oldname="$i"
 	newname="${oldname/.sav/.srm}"
-	#if [ -e "$destination/$newname" ]; then echo "Backing up $newname before updating..."; cp "$destination/$newname" "$destination/backup/$newname"; fi
+	if [ -e "$destination/$newname" ]; then echo "Backing up $newname before updating..."; cp "$destination/$newname" "$destination/backup/$newname"; fi
 	mv -v "$oldname" "$destination/$newname"
 done
